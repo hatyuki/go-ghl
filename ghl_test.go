7dcf1a42515d262db3975a84272479042375e848
hatyuki <hatyuki29@gmail.com>
2016-11-22 17:05:31 +0900

    Fix broken test

--- ghl_test.go
+++ ghl_test.go
@@ -31,35 +31,35 @@ func TestGHL_GetDownloadURL(t *testing.T) {
 		t.Errorf("returns wrong URL: got %v want %v", got, "")
 	}
 
-	if got, err := g.GetDownloadURL("hatyuki/ghl@foo", "", ""); err == nil {
+	if got, err := g.GetDownloadURL("hatyuki/go-ghl@foo", "", ""); err == nil {
 		t.Error("should return an error")
 	} else if got != "" {
 		t.Errorf("returns wrong URL: got %v want %v", got, "")
 	}
 
-	if got, err := g.GetDownloadURL("hatyuki/ghl", "", ""); err != nil {
+	if got, err := g.GetDownloadURL("hatyuki/go-ghl", "", ""); err != nil {
 		t.Errorf("returns an error: got %v", err)
 	} else if got == "" {
 		t.Error("should return URL")
-	} else if strings.Contains(got, runtime.GOOS) && strings.Contains(got, runtime.GOARCH) {
+	} else if !(strings.Contains(got, runtime.GOOS) && strings.Contains(got, runtime.GOARCH)) {
 		t.Errorf("returns wrong URL: got %v", got)
 	}
 
 	os := "linux"
 	arch := "386"
-	if got, err := g.GetDownloadURL("hatyuki/ghl", os, arch); err != nil {
+	if got, err := g.GetDownloadURL("hatyuki/go-ghl", os, arch); err != nil {
 		t.Errorf("returns an error: got %v", err)
 	} else if got == "" {
 		t.Error("should return URL")
-	} else if strings.Contains(got, os) && strings.Contains(got, arch) {
-		t.Errorf("returns wrong: got %v", got)
+	} else if !(strings.Contains(got, os) && strings.Contains(got, arch)) {
+		t.Errorf("returns wrong URL: got %v", got)
 	}
 
-	if got, err := g.GetDownloadURL("hatyuki/ghl@0.0.1", "", ""); err != nil {
+	if got, err := g.GetDownloadURL("hatyuki/go-ghl@v0.0.1", "", ""); err != nil {
 		t.Errorf("returns an error: got %v", err)
 	} else if got == "" {
 		t.Error("should return URL")
-	} else if tag := "0.0.1"; strings.Contains(got, tag) {
+	} else if tag := "v0.0.1"; !strings.Contains(got, tag) {
 		t.Errorf("returns wrong URL: got %v", got)
 	}
 }
